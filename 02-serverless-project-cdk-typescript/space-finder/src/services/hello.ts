edd62edb32f72c1b5f798dde6b975aba71a0b774
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-11 10:22:11 -1000

    Add AWS sdk and test buckets output

--- 02-serverless-project-cdk-typescript/space-finder/src/services/hello.ts
+++ 02-serverless-project-cdk-typescript/space-finder/src/services/hello.ts
@@ -1,17 +1,18 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from "aws-lambda"
 import { v4 } from 'uuid'
+import { S3Client, ListBucketsCommand } from "@aws-sdk/client-s3";
 
-// exports.main = async function (event, context) {
-//     return {
-//         statusCode: 200,
-//         body: JSON.stringify(`Hello! Reading from ${process.env.TABLE_NAME}`)
-//     }
-// }
+const s3Client = new S3Client({});
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
+    
+    const command = new ListBucketsCommand({});
+    const listBucketsResult = (await s3Client.send(command)).Buckets;
+
     const response: APIGatewayProxyResult = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!, the UUID is' + v4())
+        // body: JSON.stringify('Hello from Lambda!, the UUID is' + v4(), 'the buckets are:' + listBucketsResult);
+        body: JSON.stringify('Hello from Lambda! The buckets are:' + JSON.stringify(listBucketsResult))
     }
     console.log(event);
 
