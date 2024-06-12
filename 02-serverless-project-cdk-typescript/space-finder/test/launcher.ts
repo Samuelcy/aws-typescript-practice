73cf2e1e6ac9a2072b6abb472e489c7da46ecb77
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-11 19:36:43 -1000

    Add DynamoDb Delete

--- 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
+++ 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
@@ -4,11 +4,11 @@ process.env.AWS_REGION = "us-west-1";
 process.env.TABLE_NAME = "SpaceTable-06c1b170b487"
 
 handler({
-    httpMethod: 'PUT',
+    httpMethod: 'DELETE',
     queryStringParameters: {
         id: '4957fca7-69b2-4ca3-9d01-70427d08fc6f'
     },
-    body: JSON.stringify({
-        location: 'Dublin updated'
-    })
+    // body: JSON.stringify({
+    //     location: 'Dublin updated'
+    // })
 } as any, {} as any)
