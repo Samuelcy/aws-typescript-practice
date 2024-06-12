7f32e44a732278133e7b7dd0e24551bb94e29399
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-12 12:41:33 -1000

    Add token verification for API

--- 02-serverless-project-cdk-typescript/space-finder/src/infra/Launcher.ts
+++ 02-serverless-project-cdk-typescript/space-finder/src/infra/Launcher.ts
@@ -9,7 +9,11 @@ const dataStack = new DataStack(app, 'DataStack');
 const lambdaStack = new LambdaStack(app, 'LambdaStack', {
     spacesTable: dataStack.spacesTable
 })
+
+const authStack = new AuthStack(app, 'AuthStack');
+
 new ApiStack(app, 'ApiStack', {
-    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration
+    spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
+    userPool: authStack.userPool
+
 });
-new AuthStack(app, 'AuthStack');
--- 02-serverless-project-cdk-typescript/space-finder/test/auth.test.ts
+++ 02-serverless-project-cdk-typescript/space-finder/test/auth.test.ts
@@ -1,16 +1,3 @@
-// import { AuthService } from "./AuthService";
-
-// async function testAuth(){
-//     const service = new AuthService();
-//     const loginResult = await service.login(
-//         'barosanu',
-//         'Barosanu1!)'
-//     )
-//     console.log(loginResult);
-// }
-
-// testAuth();
-
 import { fetchAuthSession } from '@aws-amplify/auth';
 import { AuthService } from './AuthService';
  
@@ -19,7 +6,7 @@ async function testAuth() {
   await service.login('barosanu', 'Barosanu1!');
  
   const { idToken } = (await fetchAuthSession()).tokens ?? {};
- 
+
   console.log(idToken?.toString());
  
   return idToken;
