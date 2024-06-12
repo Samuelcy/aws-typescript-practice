0b48cf1b8a3ae0a6315ae8eedec7350acd28a15f
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-11 17:34:34 -1000

    Add dynamodb

--- 02-serverless-project-cdk-typescript/space-finder/src/infra/stacks/LambdaStack.ts
+++ 02-serverless-project-cdk-typescript/space-finder/src/infra/stacks/LambdaStack.ts
@@ -27,6 +27,14 @@ export class LambdaStack extends Stack {
             }
         })
 
+        spacesLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            resources: [props.spacesTable.tableArn],
+            actions: [
+                'dynamodb:PutItem'
+            ]
+        }))
+
         // helloLambda.addToRolePolicy(new PolicyStatement({
         //     effect: Effect.ALLOW,
         //     actions:[
