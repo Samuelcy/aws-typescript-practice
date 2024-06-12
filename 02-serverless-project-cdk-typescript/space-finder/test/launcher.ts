9d77359486efa0690650e3e7eb409e6a9c1619a1
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-11 17:53:10 -1000

    Add testing query

--- 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
+++ 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
@@ -1,3 +1,11 @@
-import { handler } from "../src/services/hello";
+import { handler } from "../src/services/spaces/handler";
 
-handler({} as any,{} as any)
+process.env.AWS_REGION = "us-west-1";
+process.env.TABLE_NAME = "SpaceTable-06c1b170b487"
+
+handler({
+    httpMethod: 'POST',
+    body: JSON.stringify({
+        location: 'Dublin'
+    })
+} as any,{} as any)
