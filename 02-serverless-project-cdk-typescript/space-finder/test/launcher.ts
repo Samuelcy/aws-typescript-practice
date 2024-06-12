9ebaf01b66966766f4bac22a008e93d93c8abe5b
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-11 20:05:54 -1000

    Add data queries

--- 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
+++ 02-serverless-project-cdk-typescript/space-finder/test/launcher.ts
@@ -4,18 +4,13 @@ process.env.AWS_REGION = "us-west-1";
 process.env.TABLE_NAME = "SpaceTable-06c1b170b487"
 
 handler({
-    //     httpMethod: 'DELETE',
-    //     queryStringParameters: {
-    //         id: '4957fca7-69b2-4ca3-9d01-70427d08fc6f'
-    //     },
-    //     // body: JSON.stringify({
-    //     //     location: 'Dublin updated'
-    //     // })
-    // } as any, {} as any)
-    httpMethod: 'POST',
-    body: JSON.stringify({
-        location: 'Dublin updated'
-    })
-} as any, {} as any).then(result => {
-    console.log(result)
-});
+        httpMethod: 'PUT',
+        queryStringParameters: {
+            id: 'e0f8d09c-e1d2-4e2a-83fc-23597f68c9db'
+        },
+        body: JSON.stringify({
+            location: 'Not updated'
+        })
+    } as any , {} as any).then(result =>{
+        console.log(result)
+    });
