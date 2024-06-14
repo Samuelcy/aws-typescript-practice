d7779ddc5b201eca7a362879baf5f043d17014b0
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-14 09:40:15 -1000

    Add cdk pipeline

--- 03-aws-cicd-code-pipeline/cdk-cicd/lib/cdk-cicd-stack.ts
+++ 03-aws-cicd-code-pipeline/cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,8 +1,22 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'CDKPipeline', {
+      pipelineName: 'CDKPipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub('Samuelcy/aws-typescript-practice', 'main'),
+        commands: [
+          'cd 03-aws-cicd-code-pipeline/cdk-cicd',
+          'npm ci',
+          'npx cdk synth'
+        ],
+        primaryOutputDirectory: 'cdk.out' 
+      })
+    });
   }
 }
