4b91ad08e76f07baf9c054a8fc96a704a514a72b
Samuelcy <50973399+Samuelcy@users.noreply.github.com>
2024-06-14 10:44:18 -1000

    Add automatic testing

--- 03-aws-cicd-code-pipeline/cdk-cicd/test/cdk-cicd.test.ts
+++ 03-aws-cicd-code-pipeline/cdk-cicd/test/cdk-cicd.test.ts
@@ -1,3 +1,9 @@
-test('initial test', () => {
+import { handler } from "../services/hello"
 
-});
+describe('Hello describe test suite.', ()=>{
+
+    test('Handler return 200',async ()=>{
+        const result = await handler({}, {})
+        expect(result.statusCode).toBe(200);
+    })
+})
